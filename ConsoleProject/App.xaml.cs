f7f462eb891ca92d36487fae1c486b04a8a40441
pengwei <pengweiaini123@163.com>
2024-06-29 10:11:40 +0800

    控制台应用程序

--- ConsoleProject/App.xaml.cs
+++ ConsoleProject/App.xaml.cs
@@ -1,14 +1,1 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-
-namespace ConsoleProject
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-
-}
+﻿
